fa903a98a3da189beb2974c8f4cc6c485a019ec3
ReinoMedia Digital Services <50780280+babesespresso@users.noreply.github.com>
2026-04-08 00:37:43 -0600

    Change COEP to credentialless to allow cross-origin resources

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ const nextConfig = {
           },
           {
             key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
+            value: 'credentialless',
           },
         ],
       },
